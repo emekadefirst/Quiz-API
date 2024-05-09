6b00b56d8b7c42bc77507f3c91e032bbbafca4c9
President Carter <olabodebalogun80@gmail.com>
2024-05-09 17:00:59 +0100

    template almost complete

--- app/quiz/routes.py
+++ app/quiz/routes.py
@@ -2,6 +2,7 @@ from django.urls import path
 from quiz.quiz.views import QuizView
 from quiz.question.views import QuestionView
 from quiz.attempt.views import AttemptView
+from quiz.auth.views import LoginUser, RegisterUser
 from quiz.answer.views import AnswerView, UserAnswerView
 
 urlpatterns = [
@@ -10,4 +11,6 @@ urlpatterns = [
     path('user-answer', UserAnswerView.as_view(), name='useranswer'),
     path('attempt', AttemptView.as_view(), name='attempt'),    
     path('quiz', QuizView.as_view(), name='quiz'),
+    path('login', LoginUser.as_view(), name='login'),
+    path('register', RegisterUser.as_view(), name='register'),
 ]
