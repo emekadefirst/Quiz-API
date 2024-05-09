1832911062b04bec1e91cd220efb4811a38eba27
President Carter <olabodebalogun80@gmail.com>
2024-05-09 13:54:31 +0100

    User-answer and answer endpoint completed

--- app/quiz/answer/serializers.py
+++ app/quiz/answer/serializers.py
@@ -1,2 +1,31 @@
 from rest_framework import serializers
-from .models import Answer
+from .models import Answer, UserAnswer
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
+    def create(self, validated_data):
+        answer = Answer.objects.create(
+            is_correct=validated_data['is_correct'],
+            question=validated_data['is_correct'],
+            answer_text=validated_data['answer_text']
+        )
+        return answer
+
+
+class UserAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAnswer
+        fields = '__all__'
+
+
+    def create(self, validated_data):
+        user_answer = UserAnswer.objects.create(
+            user=validated_data.get('user'),
+            is_correct=validated_data.get('is_correct'),
+            question=validated_data.get('question'),
+            answer=validated_data.get('answer')
+        )
+        return user_answer
--- app/quiz/answer/views.py
+++ app/quiz/answer/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .serializers import AnswerSerializer, UserAnswerSerializer
+from .models import Answer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class AnswerView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Answer, pk=pk, status=status.HTTP_200_OK)
+
+    def get(self, request, format=None):
+        answer = Answer.objects.all()
+        serializer = AnswerSerializer(answer, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class UserAnswerView(APIView):
+    def post(self, request, format=None):
+        serializer = UserAnswerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        
+
