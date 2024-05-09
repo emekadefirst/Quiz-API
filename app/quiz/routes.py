5d27e364cb010cf724122e4997bb11f480d11570
President Carter <olabodebalogun80@gmail.com>
2024-05-09 14:54:16 +0100

    Quiz done

--- app/quiz/routes.py
+++ app/quiz/routes.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('answer', AnswerView.as_view(), name='questionview'),
     path('user-answer', UserAnswerView.as_view(), name='useranswer'),
     path('attempt', AttemptView.as_view(), name='attempt'),    
+    path('quiz', QuizView.as_view(), name='quiz'),
 ]
