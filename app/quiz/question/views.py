2a8613ded2f28432943981dd152db73fa4766b7e
President Carter <olabodebalogun80@gmail.com>
2024-05-09 17:08:30 +0100

    simple shuffle implemented

--- app/quiz/question/views.py
+++ app/quiz/question/views.py
@@ -1,3 +1,5 @@
+import random
+from hashlib import sha256
 from django.shortcuts import get_object_or_404
 from .serializers import QuestionSerializer
 from .models import Question
@@ -5,15 +7,27 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-
 class QuestionView(APIView):
     def get_object(self, pk):
         return get_object_or_404(Question, pk=pk)
 
     def get(self, request, format=None):
         questions = Question.objects.all()
-        serializer = QuestionSerializer(questions, many=True)
+        user_id = request.user.id  # Assuming user ID is used as a unique identifier
+        shuffled_questions = self.shuffle_questions(user_id, questions)
+        serializer = QuestionSerializer(shuffled_questions, many=True)
         return Response(serializer.data)
+
+    def shuffle_questions(self, user_id, questions):
+        # Generate a unique seed for shuffling based on user ID and a secret key
+        secret_key = "your_secret_key"  # Change this to your secret key
+        seed = sha256(f"{user_id}-{secret_key}".encode()).hexdigest()
+        random.seed(seed)
+
+        # Shuffle the questions
+        shuffled_questions = random.sample(questions, len(questions))
+
+        return shuffled_questions
 
     def post(self, request, format=None):
         serializer = QuestionSerializer(data=request.data)
@@ -21,4 +35,3 @@ class QuestionView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
--- app/quiz/shuffle.py
+++
@@ -1,9 +0,0 @@
-import enum
-import random as r
-
-class Shuffle(enum.Enum):
-    def __init__(self, data):
-        self.data = data
-        
-    def shuff(self):
-        pass
