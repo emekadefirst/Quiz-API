b5b4fd2dddccbd89a952f92e052b4eb30fa4316a
President Carter <olabodebalogun80@gmail.com>
2024-05-09 17:18:22 +0100

    settings

--- app/app/settings/local.py
+++ app/app/settings/local.py
@@ -1,8 +1,23 @@
 from .base import *
 
+SECRET_KEY = os.environ.get("SECRET_KEY_LOCAL")
+
+ALLOWED_HOSTS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'local.db',
+        'NAME': BASE_DIR / 'data.db',
     }
 }
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
